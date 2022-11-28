41a888bab096c21b27a0cb4899364ea5a3dda409
林晨 <506430249@qq.com>
2022-11-28 19:38:42 +0800

    MyBatisGenerator会覆盖掉自己写的查询，很烦

--- ShareStudy-BackEnd/src/main/java/cn/tongji/study/ShareStudyBackEndApplication.java
+++ ShareStudy-BackEnd/src/main/java/cn/tongji/study/ShareStudyBackEndApplication.java
@@ -3,9 +3,10 @@ package cn.tongji.study;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @MapperScan("cn.tongji.study.mapper")
+@EnableCaching//开启缓存
 public class ShareStudyBackEndApplication {
 
     public static void main(String[] args) {
